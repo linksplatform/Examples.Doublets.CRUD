433684fde8acd1110ee297d0ceaa76ccc43c3bfb
Константин Дьяченко <konard@me.com>
2019-07-30 12:43:00 +0300

    Initial code commit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Platform.Data.Doublets;
+using Platform.Data.Doublets.ResizableDirectMemory;
+
+namespace HelloWorld.Doublets.DotNet
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var links = new ResizableDirectMemoryLinks<uint>("db.links"))
+            {
+                var link = links.Create();
+                link = links.Update(link, link, link);
+                Console.WriteLine("Hello World!");
+                Console.WriteLine($"This is my first link: ({link}:{links.GetSource(link)}->{links.GetTarget(link)}).");
+            }
+        }
+    }
+}
