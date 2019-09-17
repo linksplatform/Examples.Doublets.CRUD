faac0624e5b12176dd2a7b3563aea243aa5ba6a1
Константин Дьяченко <konard@me.com>
2019-09-17 22:27:15 +0300

    Fixed a bug of not resetting values before deletion. Added output of total links in the storage.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Platform.Data;
 using Platform.Data.Doublets;
 using Platform.Data.Doublets.ResizableDirectMemory;
 
@@ -14,6 +15,8 @@ namespace HelloWorld.Doublets.DotNet
                 link = links.Update(link, link, link);
                 Console.WriteLine("Hello World!");
                 Console.WriteLine($"This is my first link: ({link}:{links.GetSource(link)}->{links.GetTarget(link)}).");
+                Console.WriteLine($"Total links in the storage: {links.Count()}.");
+                link = links.Update(link, default, default);
                 links.Delete(link);
             }
         }
