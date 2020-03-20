1823cf96d58ec9a7956174c54e730d7e62f1b2cd
Konstantin Diachenko <drakonard@gmail.com>
2020-03-20 21:57:07 +0300

    Build fix.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Platform.Data;
 using Platform.Data.Doublets;
-using Platform.Data.Doublets.ResizableDirectMemory.Generic;
+using Platform.Data.Doublets.Memory.United.Generic;
 
 namespace HelloWorld.Doublets.DotNet
 {
@@ -9,12 +9,12 @@ namespace HelloWorld.Doublets.DotNet
     {
         static void Main()
         {
-            using (var links = new ResizableDirectMemoryLinks<uint>("db.links"))
+            using (var links = new UnitedMemoryLinks<uint>("db.links"))
             {
                 var link = links.Create();
                 link = links.Update(link, link, link);
                 Console.WriteLine("Hello World!");
-                Console.WriteLine($"This is my first link: ({link}: {links.GetSource(link)}->{links.GetTarget(link)}).");
+                Console.WriteLine($"This is my first link: {links.Format(link)}");
                 Console.WriteLine($"Total links in the storage: {links.Count()}.");
                 link = links.Update(link, default, default);
                 links.Delete(link);
