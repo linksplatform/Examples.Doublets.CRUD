88a91ac414b70a2bd2755124b4ae2d31600c9474
Konstantin Dyachenko <konard@me.com>
2021-05-05 14:43:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,27 +3,27 @@ using Platform.Data;
 using Platform.Data.Doublets;
 using Platform.Data.Doublets.Memory.United.Generic;
 
-namespace HelloWorld.Doublets.DotNet
-{
-    class Program
-    {
-        static void Main()
-        {
-            using (var links = new UnitedMemoryLinks<uint>("db.links"))
-            {
-                var link = links.Create();
-                link = links.Update(link, link, link);
-                Console.WriteLine("Hello World!");
-                Console.WriteLine($"The number of links in the the data store is {links.Count()}.");
-                Console.WriteLine("Data store contents:");
-                var query = new Link<uint>(links.Constants.Any, links.Constants.Any, links.Constants.Any);
-                links.Each((link) => {
-                    Console.WriteLine($"{links.Format(link)}");
-                    return links.Constants.Continue;
-                }, query);
-                link = links.Update(link, default, default);
-                links.Delete(link);
-            }
-        }
-    }
-}
+using var links = new UnitedMemoryLinks<uint>("db.links");
+
+Console.WriteLine("Hello World!");
+
+// A creation of the doublet link: 
+var link = links.Create();
+
+// The link is updated to reference itself twice (as a source and a target):
+link = links.Update(link, link, link);
+
+// Read operations:
+Console.WriteLine($"The number of links in the the data store is {links.Count()}.");
+Console.WriteLine("Data store contents:");
+var query = new Link<uint>(links.Constants.Any, links.Constants.Any, links.Constants.Any);
+links.Each((link) => {
+	Console.WriteLine($"{links.Format(link)}");
+	return links.Constants.Continue;
+}, query);
+
+// The link's content reset:
+link = links.Update(link, default, default);
+
+// The link deletion:
+links.Delete(link);
