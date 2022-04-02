3adb5e35aa3f2c404bdec63cff524fc32ff95a29
Konstantin Dyachenko <drakonard@gmail.com>
2022-04-02 15:42:04 +0300

    Update Program.cs

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -3,28 +3,30 @@ using Platform.Data;
 using Platform.Data.Doublets;
 using Platform.Data.Doublets.Memory.United.Generic;
 
-// A doublet links store is mapped to "db.links" file:
+// A doublet links store is mapped to the "db.links" file:
 using var links = new UnitedMemoryLinks<uint>("db.links");
 
-// A creation of the doublet link: 
+// Creating a doublet link: 
 var link = links.Create();
 
-// The link is updated to reference itself twice (as a source and a target):
+// The link is updated to reference itself twice (as a source and as a target):
+// The passed arguments are: an updated address, a new source, and a new target
 link = links.Update(link, newSource: link, newTarget: link);
 
 // Read operations:
 Console.WriteLine($"The number of links in the data store is {links.Count()}.");
 Console.WriteLine("Data store contents:");
-var any = links.Constants.Any; // Means any link address or no restriction on link address
-// Arguments of the query are interpreted as restrictions
+// Means any link address or that there is no restriction on link address
+var any = links.Constants.Any; 
+// The arguments of a query are restrictions: on address, on source, on target
 var query = new Link<uint>(index: any, source: any, target: any);
 links.Each((link) => {
     Console.WriteLine(links.Format(link));
     return links.Constants.Continue;
 }, query);
 
-// The link's content reset:
+// Cleaning (resetting) the contents of the link:
 link = links.Update(link, newSource: default, newTarget: default);
 
-// The link deletion:
+// Removing the link
 links.Delete(link);
